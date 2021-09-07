1f81f4d0511b2df91567d75cc1b97c5cf1a28ab5
Azat <gubaevazat@gmail.com>
2021-09-07 14:05:50 +0300

    completed 02

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -9,8 +9,8 @@
 # Результат проверки вывести на консоль (ДА/НЕТ)
 # Использовать только операторы if/elif/else, можно вложенные
 
-envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# envelop_x, envelop_y = 10, 7
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
@@ -19,7 +19,10 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 11, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
-
+# if paper_y <= envelop_y and paper_x <= envelop_x:
+#     print('Да')
+# else:
+#     print('НЕТ')
 # TODO здесь ваш код
 
 # Усложненное задание, решать по желанию.
@@ -28,7 +31,7 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
@@ -43,10 +46,25 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 5, 3, 6
 # brick_x, brick_y, brick_z = 11, 3, 6
 # brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
+brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
-# (просто раскоментировать нужную строку и проверить свой код)
+# (просто раcкомментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
+if hole_x >= brick_x:
+    if hole_y >= brick_y or hole_y >= brick_z:
+        print('YES')
+    else:
+        print('NO')
+elif hole_x >= brick_y:
+    if hole_y >= brick_x or hole_y >= brick_z:
+        print('YES')
+    else:
+        print('NO')
+elif hole_x >= brick_z:
+    if hole_y >= brick_y and hole_y >= brick_x:
+        print('YES')
+    else:
+        print('NO')
