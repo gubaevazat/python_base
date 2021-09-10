6b3d69b91c61dc5e11775f6f6f07bde26c64b72d
Azat <gubaevazat@gmail.com>
2021-09-11 00:01:38 +0300

    completed 07

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-
-# TODO здесь ваш код
-
-simple_draw.pause()
+for y in range(0, 601, 50):
+    sd.line(sd.get_point(0, y), sd.get_point(600, y), width=2)
+    for x in range(y % 100, 601, 100):
+        sd.line(sd.get_point(x, y), sd.get_point(x, y + 50), width=2)
+sd.pause()
