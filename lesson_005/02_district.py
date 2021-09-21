d152f85d01fc8a63b380274165426ab93427e17e
Azat <gubaevazat@gmail.com>
2021-09-21 18:31:23 +0300

    completed 02

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -7,6 +7,19 @@
 
 # TODO здесь ваш код
 
+from district.soviet_street.house1 import room1 as sov_h1_r1
+from district.soviet_street.house1 import room2 as sov_h1_r2
+from district.soviet_street.house2 import room1 as sov_h2_r1
+from district.soviet_street.house2 import room2 as sov_h2_r2
+
+from district.central_street.house1 import room1 as cen_h1_r1
+from district.central_street.house1 import room2 as cen_h1_r2
+from district.central_street.house2 import room1 as cen_h2_r1
+from district.central_street.house2 import room2 as cen_h2_r2
+
+all_folks = sov_h1_r1.folks + sov_h1_r2.folks + sov_h2_r1.folks + sov_h2_r2.folks + \
+            cen_h1_r1.folks + cen_h1_r2.folks + cen_h2_r1.folks + cen_h2_r2.folks
+print('На районе живут', ','.join(all_folks))
 
 
 
