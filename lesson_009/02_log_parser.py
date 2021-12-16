6c2b50fc751aed080de9838d59d12326335acf6a
Azat <gubaevazat@gmail.com>
2021-12-16 18:16:35 +0300

    02_log_parser 1st version

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -20,7 +20,37 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+from datetime import date, datetime
 
+
+class LogParser:
+
+    def __init__(self, file_name):
+        self.stat = {}
+        self.file_name = file_name
+
+    def collect(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file_read, \
+                open('events_NOK.txt', 'w', encoding='cp1251') as file_write:
+            date_time_current = None
+            count = 0
+            for line in file_read:
+                log_line = line.replace('[', '').replace(']', '').split()
+                date_time_next = datetime.strptime(log_line[0] + ' ' + log_line[1], '%Y-%m-%d %H:%M:%S.%f')
+                date_time_next = date_time_next.replace(second=0, microsecond=0)
+                if log_line[2] == 'NOK':
+                    if date_time_current == date_time_next:
+                        count += 1
+                    else:
+                        if count != 0:
+                            line = '[' + date_time_next.strftime('%Y-%m-%d %H:%M') + '] ' + str(count) + '\n'
+                            file_write.write(line)
+                        date_time_current = date_time_next
+                        count = 0
+
+
+parser = LogParser('events.txt')
+parser.collect()
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
