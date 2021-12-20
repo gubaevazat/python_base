e884fb06e9dfca57a1aa7d4304b04871be8ed56d
Azat <gubaevazat@gmail.com>
2021-12-20 14:21:44 +0300

    02_log_parser completed

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,7 +23,7 @@
 from datetime import date, datetime
 
 
-class LogParser:
+class LogParserMinute:
 
     def __init__(self, file_name):
         self.stat = {}
@@ -32,24 +32,58 @@ class LogParser:
     def collect(self):
         with open(self.file_name, 'r', encoding='cp1251') as file_read, \
                 open('events_NOK.txt', 'w', encoding='cp1251') as file_write:
-            date_time_current = None
-            count = 0
+
             for line in file_read:
                 log_line = line.replace('[', '').replace(']', '').split()
-                date_time_next = datetime.strptime(log_line[0] + ' ' + log_line[1], '%Y-%m-%d %H:%M:%S.%f')
-                date_time_next = date_time_next.replace(second=0, microsecond=0)
                 if log_line[2] == 'NOK':
+                    date_time_current = datetime.strptime(log_line[0] + ' ' + log_line[1], '%Y-%m-%d %H:%M:%S.%f')
+                    date_time_current = self.sort(date_time_current)
+                    # print(date_time_current)
+                    count = 1
+                    break
+
+            for line in file_read:
+                log_line = line.replace('[', '').replace(']', '').split()
+                if log_line[2] == 'NOK':
+                    date_time_next = datetime.strptime(log_line[0] + ' ' + log_line[1], '%Y-%m-%d %H:%M:%S.%f')
+                    date_time_next = self.sort(date_time_next)
+                    # print(date_time_next)
                     if date_time_current == date_time_next:
                         count += 1
                     else:
-                        if count != 0:
-                            line = '[' + date_time_next.strftime('%Y-%m-%d %H:%M') + '] ' + str(count) + '\n'
-                            file_write.write(line)
+                        line = '[' + self.format_write(date_time_current) + '] ' + str(count) + '\n'
+                        file_write.write(line)
                         date_time_current = date_time_next
-                        count = 0
+                        count = 1
+
+    @staticmethod
+    def sort(date_time):
+        return date_time.replace(second=0, microsecond=0)
+
+    @staticmethod
+    def format_write(date_time):
+        return date_time.strftime('%Y-%m-%d %H:%M')
 
 
-parser = LogParser('events.txt')
+class LogParserHour(LogParserMinute):
+
+    @staticmethod
+    def sort(date_time):
+        return date_time.replace(minute=0, second=0, microsecond=0)
+
+
+class LogParserDay(LogParserMinute):
+
+    @staticmethod
+    def sort(date_time):
+        return date_time.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    @staticmethod
+    def format_write(date_time):
+        return date_time.strftime('%Y-%m-%d')
+
+
+parser = LogParserDay('events.txt')
 parser.collect()
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
