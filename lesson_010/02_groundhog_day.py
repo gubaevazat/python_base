796e81a85e5c3f2cd9f8c873f7cbbb6f4fde6e32
Azat <gubaevazat@gmail.com>
2022-02-17 17:53:29 +0300

    02_groundhog_day complete

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,72 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+from random import randint
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+
+    def __init__(self, message='Я бог!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DrunkError(Exception):
+
+    def __init__(self, message='Я пьян!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class CarCrashError(Exception):
+
+    def __init__(self, message='Я разбил машину'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class GluttonyError(Exception):
+
+    def __init__(self, message='Я объелся!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DepressionError(Exception):
+
+    def __init__(self, message='Я в депрессии!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SuicideError(Exception):
+
+    def __init__(self, message='Я самоубийца!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+def one_day():
+    dice = randint(1, 13)
+    if dice == 1:
+        raise my_errors[randint(0, 5)]
+    else:
+        return randint(1, 7)
+
+
+my_errors = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+
+carma = 0
+while True:
+    try:
+        carma += one_day()
+    except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exc:
+        print(f'Что то пошло не так - {exc}')
+    if carma >= ENLIGHTENMENT_CARMA_LEVEL:
+        break
+
+
 
 # https://goo.gl/JnsDqu
